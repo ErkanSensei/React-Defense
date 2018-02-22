6d32f447d0f49cdbc20dc9cd7bd6ae0fbacfa822
ErkanSensei <erkansen14@hotmail.com>
2018-02-21 19:12:16 -0600

    added path coordinates and centered enemy

--- src/App.js
+++ src/App.js
@@ -8,37 +8,53 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      paths: [],
+    };
     this.grid = [
-      <div className='ice' />,
-      <div className='ice' />,
-      <div className='path' />,
-      <div className='path' />,
-      <div className='ice' />,
-      <div className='path' />,
-      <div className='path' />,
-      <div className='ice' />,
-      <div className='path' />,
-      <div className='path' />,
-      <div className='ice' />,
-      <div className='ice' />,
-    ]
+      <div className="ice" />,
+      <div className="ice" />,
+      <div className="path" />,
+      <div className="path" />,
+      <div className="ice" />,
+      <div className="path" />,
+      <div className="path" />,
+      <div className="ice" />,
+      <div className="path" />,
+      <div className="path" />,
+      <div className="ice" />,
+      <div className="ice" />,
+    ];
+  }
+
+  componentDidMount() {
+    if (!this.state.paths.length) {
+      const paths = [].slice.call(document.getElementsByClassName('path'));
+      console.log(paths)
+      this.setState({paths});
+    }
   }
   render() {
     return (
       <div className="middle">
         <div className="App">
-          {this.grid.map(square => {
-            return square
+          {this.grid.map((square, index) => {
+            return square;
           })}
-          {/* <SpriteSheet
-            className='spriteSheet'
+          {this.state.paths.length && <SpriteSheet
+            style={{
+              width: '10%',
+              position: 'absolute',
+              left: this.state.paths[4].offsetLeft + this.state.paths[4].clientWidth / 2,
+              top: this.state.paths[4].offsetTop + this.state.paths[4].clientHeight / 2
+            }}
             image={BigBlobSpriteSheet}
             widthFrame={32}
             heightFrame={24}
             steps={10}
             fps={12}
             loop
-          /> */}
+          />}
         </div>
       </div>
     );
